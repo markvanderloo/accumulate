ed5671b4b9bfbc2422106f32637537529a6660eb
Mark van der Loo <mark.vanderloo@gmail.com>
2022-10-29 13:00:01 +0200

    added some helper functions to facilitate specifying demands

---
+++ pkg/R/helpers.R
@@ -0,0 +1,149 @@
+#' Demand minimal number of functons
+#' 
+#' @param n Minimal number of records in a group.
+#'
+#' @examples
+#'
+#' min_records(5)(women)
+#' min_records(200)(women)
+#'
+#' @family helpers
+#'
+#' @export
+min_records <- function(n){
+  min_rec <- n
+  function(d) if (nrow(d) < n) FALSE else TRUE
+}
+
+#' Demand maximal number of records
+#'
+#' @param n Minimal number of records in a group.
+#'
+#' @examples
+#'
+#' max_records(5)(women)
+#' max_records(200)(women)
+#'
+#'
+#' @family helpers
+#'
+#' @export
+max_records <- function(n){
+  max_rec <- n
+  function(d) if (nrow(d) >n) FALSE else TRUE
+}
+
+#' Demand any number of expressions to be TRUE
+#'
+#' @param ... comma-separated list of expressions that must all evaluate to \code{TRUE}
+#'            in order for a data set to be acceptable.
+#'
+#' @return A data-checking function.
+#' @family helpers
+#' @export
+demand <- function(...){
+  expr <- as.list(substitute(list(...))[-1])  
+  function(d){
+    L <- lapply(expr, function(e) with(d, eval(e)))
+    all(sapply(L,all))
+  }
+}
+
+#' Number of incomplete combinations
+#'
+#' A number of vectors is pointwise tested for missing values.  For each
+#' position, if any vector has a missing \code{FALSE} is computed otherwise
+#' \code{TRUE}. The total number of \code{FALSE}s is returned.
+#'
+#' @param ... comma-separated list of vectors of equal length
+#'
+#' @note This function is intended to facilitate the use of \code{\link{demand}}
+#' @return A non-negative integer.
+#'
+#'
+#' @examples
+#' n_incomplete(c(1,NA,3), 1:3)
+#'
+#' 
+#' @family helpers
+#'
+#' @export
+n_incomplete <- function(...){
+  L <- list(...)
+  sum( Reduce(`|`, lapply(L,  is.na)) )
+}
+
+#' Number of complete combinations
+#'
+#' A number of vectors is pointwise tested for missing values.  For each
+#' position, if any vector has a missing \code{FALSE} is computed otherwise
+#' \code{TRUE}. The total number of \code{TRUE}s is returned.
+#'
+#' @param ... comma-separated list of vectors of equal length
+#'
+#' @note This function is intended to facilitate the use of \code{\link{demand}}
+#' @return A non-negative integer.
+#'
+#'
+#' @examples
+#' n_complete(c(1,NA,3), 1:3)
+#'
+#' 
+#' @family helpers
+#'
+#' @export
+n_complete <- function(...){
+
+  L <- list(...)
+  sum( Reduce(`&`, lapply(L,  function(x) !is.na(x) )) )
+
+}
+
+#' Fraction of incomplete combinations
+#'
+#' A number of vectors is pointwise tested for missing values. For each
+#' position, if any vector has a missing \code{FALSE} is computed otherwise
+#' \code{TRUE}. The fraction of \code{FALSE}s is returned.
+#'
+#' @param ... comma-separated list of vectors of equal length
+#'
+#' @note This function is intended to facilitate the use of \code{\link{demand}}
+#' @return A non-negative integer.
+#'
+#'
+#' @examples
+#' fraction_incomplete(c(1,NA,3), 1:3)
+#'
+#' 
+#' @family helpers
+#'
+#' @export
+fraction_incomplete <- function(...){
+  L <- list(...)
+  mean( Reduce(`|`, lapply(L,  is.na)) )
+}
+
+#' Fraction of complete combinations
+#'
+#' A number of vectors is pointwise tested for missing values.  For each
+#' position, if any vector has a missing \code{FALSE} is computed otherwise
+#' \code{TRUE}. The fraction of \code{TRUE}s is returned.
+#'
+#' @param ... comma-separated list of vectors of equal length
+#'
+#' @note This function is intended to facilitate the use of \code{\link{demand}}
+#'
+#' @return A non-negative integer.
+#'
+#' @examples
+#' fraction_complete(c(1,NA,3), 1:3)
+#'
+#' 
+#' @family helpers
+#' @export
+fraction_complete <- function(...){
+  L <- list(...)
+  mean( Reduce(`&`, lapply(L,  function(x) !is.na(x) )) )
+}
+
+
---
+++ pkg/inst/tinytest/test_helpers.R
@@ -0,0 +1,7 @@
+
+expect_equal(n_complete(c(1,NA,2), 1:3), 2)
+expect_equal(fraction_complete(c(1,NA,2), 1:3), 2/3)
+expect_equal(n_incomplete(c(1,NA,2), 1:3), 1)
+expect_equal(fraction_incomplete(c(1,NA,2), 1:3), 1/3)
+
+
