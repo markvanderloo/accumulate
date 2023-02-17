ea0f77b3c974c65c9e71e73e03829da74adc8c70
Mark van der Loo <mark.vanderloo@gmail.com>
2023-02-17 22:47:19 +0100

    added test for object_list

---
+++ pkg/inst/tinytest/test_object_list.R
@@ -0,0 +1,20 @@
+# objects of type 'object_list' are typed lists that only differ from ordenary
+# lists in the way that they are printed. Also, selecting a subset with `[`
+# returns an 'object_list'. 
+
+
+# trivial check on constructor
+expect_inherits(object_list(1:3),"object_list")
+
+# check subsetting method
+expect_inherits(object_list(1:3)[1], "object_list")
+
+
+# only the type of each element is printed
+expect_match(format(object_list(1L)), "^<integer>$")
+expect_stdout(print(object_list(1L)), "<integer>")
+
+
+
+
+
