2aa0afbc8f3d46f6404b1380c922aa392cd2f5b3
Mark van der Loo <mark.vanderloo@gmail.com>
2023-02-17 22:53:52 +0100

    adapted to CRAN version of tinytest

--- pkg/inst/tinytest/test_object_list.R
+++ pkg/inst/tinytest/test_object_list.R
@@ -11,7 +11,7 @@ expect_inherits(object_list(1:3)[1], "object_list")
 
 
 # only the type of each element is printed
-expect_match(format(object_list(1L)), "^<integer>$")
+expect_true(grepl("^<integer>$", format(object_list(1L))))
 expect_stdout(print(object_list(1L)), "<integer>")
 
 
